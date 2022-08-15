875511dac62872820dfb74d04ea1290d94e2185f
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-15 11:53:52 +0100

    create component NoPosts for when the user doesn't follow anyone

---
+++ src/components/NotPosts/index.js
@@ -0,0 +1,17 @@
+import { AlignAllCenter, Container, IconNoPosts, TitleNoPosts } from "./styles";
+import IconPosts from "../../assets/noPosts.svg";
+
+const NoPosts = () => {
+  return (
+    <>
+      <AlignAllCenter>
+        <Container>
+          <IconNoPosts src={IconPosts} />
+          <TitleNoPosts>Follow more people to see your posts</TitleNoPosts>
+        </Container>
+      </AlignAllCenter>
+    </>
+  );
+};
+
+export default NoPosts;
