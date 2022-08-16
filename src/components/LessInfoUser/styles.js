8bd6ec56f567c5d07ffe04827e573a7559571d96
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-16 11:09:00 +0100

    change view following

--- src/components/LessInfoUser/styles.js
+++ src/components/LessInfoUser/styles.js
@@ -1,38 +1,9 @@
 import styled from "styled-components";
 
-export const AllAlignCenter = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 60px;
-  margin-bottom: 140px;
-`;
-
-export const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  border-radius: 16px;
-  background-color: #303030;
-  margin: 40px 0px;
-  padding: 20px 20px;
-  min-width: 40%;
+export const ContainerNames = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-export const TitleFollowing = styled.p`
-  color: #8d8d8d;
-  font-weight: 700;
-  text-transform: uppercase;
-  text-align: center;
-  font-size: 40px;
-  margin-bottom: 0px;
-`;
-
-export const TextNumberFollowers = styled.p`
-  color: #8d8d8d;
-  font-size: 18px;
-  text-align: center;
-  margin-top: 0;
+  margin-left: 16px;
 `;
 
 export const ImageAndNames = styled.div`
@@ -45,12 +16,9 @@ export const ProfileImage = styled.img`
   border-radius: 50%;
   max-height: 100px;
   max-width: 100px;
-`;
-
-export const ContainerNames = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 16px;
+  min-height: 100px;
+  min-width: 100px;
+  object-fit: cover;
 `;
 
 export const UserName = styled.p`
--- src/pages/ViewFollowers/styles.js
+++ src/pages/ViewFollowers/styles.js
@@ -34,35 +34,3 @@ export const TextNumberFollowers = styled.p`
   text-align: center;
   margin-top: 0;
 `;
-
-export const ImageAndNames = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-`;
-
-export const ProfileImage = styled.img`
-  border-radius: 50%;
-  max-height: 100px;
-  min-height: 100px;
-  max-width: 100px;
-  min-width: 100px;
-  object-fit: cover;
-`;
-
-export const ContainerNames = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 16px;
-`;
-
-export const UserName = styled.p`
-  font-size: 16px;
-  margin-bottom: 0;
-  color: #74b3ce;
-`;
-
-export const NameProfile = styled.p`
-  margin-top: 0;
-  font-weight: 700;
-`;
