e1c0db491db40162e59ce5edf6eb46e7ff7f5898
Nuno <nuno.carmo@mindswap.academy>
2022-10-26 14:03:08 +0100

    remove console logs

--- src/services/LoginUser.js
+++ src/services/LoginUser.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import LINK from "./Link";
 
 export default async function LoginUser(credentials) {
     try {
-        const response = await axios.post("/login", credentials);
+        const response = await axios.post(LINK + "/login", credentials);
         const token = response.headers.authorization
         localStorage.setItem('token', token);
         return token;
